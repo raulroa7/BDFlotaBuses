80ecacf10543236307568b9442ab970f77b31ea0
Raul Roa <raulricardoroa@gmail.com>
2021-09-26 22:53:52 -0500

    Merge branch 'feature/initialConfiguration' into 'trunk'
    Proyecto base de datos Flota Buses

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -9,13 +9,13 @@ import static util.enums.SystemProperties.USER_DIR;
 public class Main {
 
     private static final String SERVER = "sofka-training.cpxphmd1h1ok.us-east-1.rds.amazonaws.com";
-    private static final String DATA_BASE_NAME = "ivanArroyave_cine_10092021";
+    private static final String DATA_BASE_NAME = "raulRoa_flotaBuses_10092021";
     private static final String USER = "sofka_training";
     private static final String PASSWORD = "BZenX643bQHw";
 
-    private static final String SELECT_ALL_FROM_CIUDAD = String.format("select * from %s.ciudad", DATA_BASE_NAME);
-    private static final String SELECT_ALL_FROM_DIRECTOR= String.format("select * from %s.director", DATA_BASE_NAME);
-    private static final String CALL_SP_LISTAR_PELICULAS= String.format("call sp_listar_peliculas()");
+    private static final String SELECT_ALL_FROM_CONCESIONARIO = String.format("select * from Concesionario", DATA_BASE_NAME);
+    private static final String SELECT_ALL_FROM_BUS= String.format("select * from Bus", DATA_BASE_NAME);
+    private static final String CALL_SP_MOSTRAR_CONCESIONARIOPADRON= String.format("call mostrar_concesioanrioPadron()");
 
     private static final MySqlOperations mySqlOperations = new MySqlOperations();
 
@@ -42,19 +42,19 @@ public class Main {
     }
 
     private static void selectAllFromCiudad() throws SQLException {
-        mySqlOperations.setSqlStatement(SELECT_ALL_FROM_CIUDAD);
+        mySqlOperations.setSqlStatement(SELECT_ALL_FROM_CONCESIONARIO);
         mySqlOperations.executeSqlStatement();
         mySqlOperations.printResultSet();
     }
 
     private static void selectAllFromDirector() throws SQLException {
-        mySqlOperations.setSqlStatement(SELECT_ALL_FROM_DIRECTOR);
+        mySqlOperations.setSqlStatement(SELECT_ALL_FROM_BUS);
         mySqlOperations.executeSqlStatement();
         mySqlOperations.printResultSet();
     }
 
     private static void callSpListarPeliculas() throws SQLException {
-        mySqlOperations.setSqlStatement(CALL_SP_LISTAR_PELICULAS);
+        mySqlOperations.setSqlStatement(CALL_SP_MOSTRAR_CONCESIONARIOPADRON);
         mySqlOperations.executeSqlStatement();
         mySqlOperations.printResultSet();
     }
@@ -64,3 +64,4 @@ public class Main {
     }
 
 }
+
